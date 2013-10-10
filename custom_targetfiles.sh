e950652e987790f520ae2e80d9f81bb9f6f8499c
tangliuxiang01 <tangliuxiang01@baidu.com>
2013-10-10 18:04:16 +0800

    CL-55683: reset su's permission to 6755 when restart, use 4.0's updater to fix ota update error, fix the set fb's error in recovery
    Change-Id: Ibd680361dd0a8002c6b6c904d8077ab4106f95d8

---
+++ custom_targetfiles.sh
@@ -0,0 +1,6 @@
+
+echo "in $0"
+
+OUT_DIR=out/merged_target_files/
+
+cp overlay/OTA/bin/updater $OUT_DIR/OTA/bin/updater
--- overlay/system/bin/copy-cache.sh
+++ overlay/system/bin/copy-cache.sh
@@ -1,4 +1,12 @@
 #!/system/bin/sh
 
-su -system -c "cp /data/local/tmp/command /cache/recovery/"
+result=1
+
+while [ $result != 0 ];
+do
+	su -c "cp /data/local/tmp/command /cache/recovery/"
+	result=$?
+	echo "result: $result"
+	sleep 3
+done
 exit 0
