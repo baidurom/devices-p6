3a0fe7681b01f439248e3ffcb9dceffdd567dbb9
tangliuxiang01 <tangliuxiang01@baidu.com>
2013-10-12 10:15:58 +0800

    CL-55918: custom out/server to add P6's flash bat to server
    Change-Id: Iab15123b09f676eea818a72505772dcc93de9360

--- custom_targetfiles.sh
+++ custom_targetfiles.sh
@@ -3,4 +3,8 @@ echo "in $0"
 
 OUT_DIR=out/merged_target_files/
 
+mkdir -p $OUT_DIR/OTA/bin
 cp overlay/OTA/bin/updater $OUT_DIR/OTA/bin/updater
+
+mkdir -p out/server/image
+cp -rf overlay/server/image/* out/server/image
