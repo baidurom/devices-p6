a65ad338b242a4268f9f70f36c77d3577053e1c2
zoujunhua <zoujunhua@baidu.com>
2013-10-18 10:50:53 +0800

    CL-55940: fixed the bug about the BaiduImageSearch app crash when work in the scan code mode.
    Change-Id: I688747668bf4eb6104ef32c435e7023737155892

--- custom_app.sh
+++ custom_app.sh
@@ -9,4 +9,9 @@ if [ "$apkBaseName" = "Settings" ];then
         echo ">>> begin delete unlock_set_baidu_slide in $tempSmaliDir/res/xml/security_settings_picker.xml"
         sed -i '/unlock_set_baidu_slide/d' $tempSmaliDir/res/xml/security_settings_picker.xml
     fi
+
+elif [ "$apkBaseName" = "BaiduImageSearch" ];then
+       echo ">>> in custom_app for BaiduImageSearch"
+       sed -i -e "/^\.method.*getOptimalPreviewSize(Landroid\/app\/Activity;Ljava\/util\/List;)Landroid\/hardware\/Camera\$Size/,/^\.end method/d" $tempSmaliDir/smali/com/baidu/imagesearch/utility/CameraUtility.smali
+
 fi
