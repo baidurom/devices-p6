5f4f47dbfc3bc24da2d0557a646b0011877fcfd5
zoujunhua <zoujunhua@baidu.com>
2013-11-28 21:50:30 +0800

    CL-56417: add huawei HwGlobalDolbyEffect apk and entrance in the settings apk.
    Change-Id: Ib60aa464df4477c3c3855368234f19f0a31317c7

--- custom_app.sh
+++ custom_app.sh
@@ -9,4 +9,7 @@ if [ "$apkBaseName" = "Settings" ];then
         echo ">>> begin delete unlock_set_baidu_slide in $tempSmaliDir/res/xml/security_settings_picker.xml"
         sed -i '/unlock_set_baidu_slide/d' $tempSmaliDir/res/xml/security_settings_picker.xml
     fi
+
+    sed -i '/sound_category_system_title/r Settings/sound_settings.xml.part' $tempSmaliDir/res/xml/sound_settings.xml
+
 fi
